d0fb4493b3c547ff8544c6d98af9451420585b91
june960427 <ljh975896@gmail.com>
2024-08-21 01:33:06 +0900

    config 수정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "https://june960427.github.io/Todo/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
