4289f80eacbab29dedf65601176db5a9f78dd360
june960427 <ljh975896@gmail.com>
2024-08-21 03:19:23 +0900

    vanilla js todo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: /Todo/,
+  base: "/Vanilla JS Todo",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
